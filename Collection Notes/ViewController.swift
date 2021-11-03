3d2ebac4b769bfd458bdeb5322a053616cabec64
Daniel Vega <drvega7@icloud.com>
2021-11-02 22:17:43 -0500

    collection notes

--- Collection Notes/ViewController.swift
+++ Collection Notes/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
 
+    @IBOutlet weak var collectionViewOutlet: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionViewOutlet.delegate = self
+        collectionViewOutlet.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "myCell", for: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
+        return cell
+    }
 
 }
 
